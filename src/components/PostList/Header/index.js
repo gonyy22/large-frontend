60238349bf8a4e137fef5e46705ec5b71c4e239f
jewook <jewook3617@gmail.com>
2021-10-17 15:49:27 +0900

    header layout ing...

--- src/components/PostList/Header/index.js
+++ src/components/PostList/Header/index.js
@@ -3,7 +3,12 @@ import './Header.css';
 function Headers() {
   return (
     <div className="headers">
-      Headerss
+      <img src="img/logo.svg" />
+      <div className="header_info">
+        <img src="img/search.svg" />
+        <p className="write_btn">글쓰기</p>
+        <p className="profile">j</p>
+      </div>
     </div>
   );
 }
