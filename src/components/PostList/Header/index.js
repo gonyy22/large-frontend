44635bd6084340dabd307fb8132b4731342698af
gonyy22 <53338566+gonyy22@users.noreply.github.com>
2021-10-18 00:37:17 +0900

    Update header

--- src/components/PostList/Header/index.js
+++ src/components/PostList/Header/index.js
@@ -3,10 +3,10 @@ import './Header.css';
 function Headers() {
   return (
     <div className="headers">
-      <img src="img/logo.svg" />
+      <img src="img/logo.svg" className="logo_img"/>
       <div className="header_info">
-        <img src="img/search.svg" />
-        <p className="write_btn">글쓰기</p>
+        <img src="img/search.png" />
+        <button className="write_btn">글쓰기</button>
         <p className="profile">j</p>
       </div>
     </div>
