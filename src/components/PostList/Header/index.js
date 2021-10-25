dd030d5a59fd987af48a35ce04d261d5ce6e8e33
gonyy22 <53338566+gonyy22@users.noreply.github.com>
2021-10-25 23:36:09 +0900

    Eslint Prettier 적용

--- src/components/PostList/Header/index.js
+++ src/components/PostList/Header/index.js
@@ -3,10 +3,12 @@ import './Header.css';
 function Headers() {
   return (
     <div className="headers">
-      <img src="img/logo.svg" className="logo_img"/>
+      <img a src="img/logo.svg" className="logo_img" />
       <div className="header_info">
-        <img src="img/search.png" />
-        <button className="write_btn">글쓰기</button>
+        <img src="img/search.png" alt="" />
+        <button type="button" className="write_btn">
+          글쓰기
+        </button>
         <p className="profile">j</p>
       </div>
     </div>
