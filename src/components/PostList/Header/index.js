f941582f84405ac69023e7faac89c89677c5871d
gonyy22 <53338566+gonyy22@users.noreply.github.com>
2021-10-26 16:26:23 +0900

    Update header

--- src/components/PostList/Header/index.js
+++ src/components/PostList/Header/index.js
@@ -1,17 +1,38 @@
 import './Header.css';
+import { useState } from 'react';
 
 function Headers() {
+  const [searchValue, setSearchValue] = useState(true);
+
+  function handleSearch() {
+    setSearchValue((prevSearchValue) => !prevSearchValue);
+  }
+
   return (
-    <div className="headers">
-      <img a src="img/logo.svg" className="logo_img" />
-      <div className="header_info">
-        <img src="img/search.png" alt="" />
-        <button type="button" className="write_btn">
-          글쓰기
-        </button>
-        <p className="profile">j</p>
+    <nav className="header">
+      <div className="inner_header">
+        <img a src="img/logo.svg" className="logo_img" alt="" />
+        <div className="header_info">
+          <div className="search_wrap">
+            <button type="button" handleSearch={handleSearch}>
+              <img src="img/search.png" alt="" />
+            </button>
+            <input
+              type="text"
+              id="search"
+              name="search"
+              placeholder="Search posts"
+            />
+          </div>
+          <div className="write_profile_wrap">
+            <button type="button" className="write_btn">
+              글쓰기
+            </button>
+            <p className="profile">j</p>
+          </div>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 }
 
