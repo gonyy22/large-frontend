eecfb727b9771d016433d36a4953b3aa5737718e
jewook <jewook3617@gmail.com>
2021-10-26 20:02:46 +0900

    Search Input에 Styled Component 적용

--- src/components/PostList/Header/index.js
+++ src/components/PostList/Header/index.js
@@ -1,16 +1,25 @@
 import './Header.css';
 import { useState } from 'react';
+import styled from 'styled-components';
+
+const SearchInput = styled.input`
+  margin-left: 10px;
+  border: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  transition: all 0.5s;
+  :focus {
+    outline: none;
+  }
+  width: ${(props) => (props.isOpen ? '160px' : '0')};
+`;
 
 function Headers() {
-  const [searchValue, setSearchValue] = useState(true);
+  const [searchValue, setSearchValue] = useState(false);
 
   function handleSearch() {
     setSearchValue((prevSearchValue) => !prevSearchValue);
-    if (searchValue === true) {
-      document.getElementById('search').style.width = '160px';
-    } else if (searchValue === false) {
-      document.getElementById('search').style.width = '0';
-    }
   }
 
   return (
@@ -22,11 +31,12 @@ function Headers() {
             <button type="button" onClick={handleSearch}>
               <img src="img/search.png" alt="" />
             </button>
-            <input
+            <SearchInput
               type="text"
               id="search"
               name="search"
               placeholder="Search posts"
+              isOpen={searchValue}
             />
           </div>
           <div className="write_profile_wrap">
