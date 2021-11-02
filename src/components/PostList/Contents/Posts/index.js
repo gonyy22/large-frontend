d545967c5d57b73111eb9312d96c521c9f8b9798
jewook <jewook3617@gmail.com>
2021-11-02 20:26:19 +0900

    setup fake data

--- src/components/PostList/Contents/Posts/index.js
+++ src/components/PostList/Contents/Posts/index.js
@@ -2,6 +2,51 @@ import './Posts.css';
 import Post from './Post';
 
 function Posts() {
+  const posts = [
+    {
+      profileImageSrc: 'abc',
+      author: '냐냐',
+      title: 'Debugging Node.js with pp(pretty-print)',
+      tag: 'front-end',
+      createdAt: '2021-09-09',
+    },
+    {
+      profileImageSrc: 'abc',
+      author: '냐냐',
+      title: 'Debugging Node.js with pp(pretty-print)',
+      tag: 'front-end',
+      createdAt: '2021-09-09',
+    },
+    {
+      profileImageSrc: 'abc',
+      author: '냐냐',
+      title: 'Debugging Node.js with pp(pretty-print)',
+      tag: 'front-end',
+      createdAt: '2021-09-09',
+    },
+    {
+      profileImageSrc: 'abc',
+      author: '냐냐',
+      title: 'Debugging Node.js with pp(pretty-print)',
+      tag: 'front-end',
+      createdAt: '2021-09-09',
+    },
+    {
+      profileImageSrc: 'abc',
+      author: '냐냐',
+      title: 'Debugging Node.js with pp(pretty-print)',
+      tag: 'front-end',
+      createdAt: '2021-09-09',
+    },
+    {
+      profileImageSrc: 'abc',
+      author: '냐냐',
+      title: 'Debugging Node.js with pp(pretty-print)',
+      tag: 'front-end',
+      createdAt: '2021-09-09',
+    },
+  ];
+  console.log(posts);
   return (
     <div className="posts_wrap">
       <Post />
--- src/components/PostList/Contents/Topics/index.js
+++ src/components/PostList/Contents/Topics/index.js
@@ -2,6 +2,15 @@ import './Topics.css';
 import Topic from './Topic/index';
 
 function Topics() {
+  const topics = [
+    { name: 'front end' },
+    { name: 'back end' },
+    { name: 'database' },
+    { name: 'devops' },
+    { name: 'AI' },
+  ];
+
+  console.log(topics);
   return (
     <>
       <p className="topics_title">내가 선택한 토픽</p>
